63506905c85e417aa832fc6dd1125931632f9419
LAUV\HP <phamnguyenvu2k5@gmail.com>
2024-07-07 01:49:15 +1000

    added scss folder

--- src/index.js
+++ src/index.js
@@ -5,21 +5,25 @@ const path = require('path');
 
 const app = express();
 const port = 3000;
-// Set up Handlebars as the template engine
 
+// Set up Handlebars as the template engine
 app.engine('.hbs', engine({ extname: '.hbs'}));
 app.set('view engine', 'hbs');
-app.set('views',path.join(__dirname,'resources/views'))
+app.set('views', path.join(__dirname, 'resources/views'));
+
+// Handle static files from the 'public' directory 
+app.use(express.static(path.join(__dirname, 'public')));
+
 // HTTP logger
 app.use(morgan('combined'));
 
 // Define routes
-app.get('/', (req, res) => {    
-    res.render('home')
+app.get('/', (req, res) => {
+    res.render('home');
 });
 
-app.get('/news', (req, res) => {    
-    res.render('news')
+app.get('/news', (req, res) => {
+    res.render('news');
 });
 
 // Start the server
