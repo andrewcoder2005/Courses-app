9ba2db66e470bd1a70d5819ca11e7d8b57ff1422
LAUV\HP <phamnguyenvu2k5@gmail.com>
2024-07-11 23:19:50 +1000

    Refactored  code: creates routes, app/controllers folders

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const morgan = require('morgan');
 const { engine } = require('express-handlebars');
 const path = require('path');
-
 const app = express();
 const port = 3000;
+
+const route = require('../src/routes/index.route');
 
 // Set up Handlebars as the template engine
 app.engine('.hbs', engine({ extname: '.hbs'}));
@@ -14,17 +15,9 @@ app.set('views', path.join(__dirname, 'resources/views'));
 // Handle static files from the 'public' directory 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// HTTP logger
-app.use(morgan('combined'));
 
-// Define routes
-app.get('/', (req, res) => {
-    res.render('home');
-});
-
-app.get('/news', (req, res) => {
-    res.render('news');
-});
+// routes initialization
+route(app);
 
 // Start the server
 app.listen(port, () => {
