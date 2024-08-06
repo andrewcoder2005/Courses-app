7bb51a4e62ec152ca9c0c0c1ee3fad52a895f8be
LAUV\HP <phamnguyenvu2k5@gmail.com>
2024-08-06 23:41:42 +1000

    Updated U/I, Read object from model

--- src/app/controllers/SitesController.js
+++ src/app/controllers/SitesController.js
@@ -2,12 +2,13 @@ const Course = require('../models/course.model');
 
 class SitesController{
     //* [GET] /home
-    index(req,res){
-        const courses = Course.find({})
-        .then((courses) => res.json(courses))
-        .catch((err) => res.status( 400).json({error:'ERROR!!!'}))
+    index(req,res,next){
+        Course.find({}).lean()
+        .then(courses => res.render('home',{courses}))
+        .catch(next);
     // res.render('home');
     }
+
     search(req,res){
         res.render('search');
     }
